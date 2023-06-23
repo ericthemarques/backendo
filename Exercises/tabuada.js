b73117d7698190422c8f988dc8d98a6326261354
ericrunnerr <eric.marquesgomesmarques@gmail.com>
2023-06-23 15:16:17 -0300

    exercício de IMC e tabuada

---
+++ Exercises/tabuada.js
@@ -0,0 +1,7 @@
+const multiplicador = 7;
+let iniciador = 0;
+
+for (let iniciador = 0; iniciador <= 10; iniciador++) {
+    const calculo = multiplicador * iniciador
+    console.log(`${multiplicador} x ${iniciador} = ${calculo}`)
+}
